4a4921d9d00c9eb479969b10e778e9ab5c8f7fd1
Nathan Grigg <nathan@nathanamy.org>
2012-09-26 07:56:13 -0700

    Remove timezone option from command line interface

--- dayone_export/cli.py
+++ dayone_export/cli.py
@@ -17,16 +17,13 @@ def parse_args(args=None):
     """Parse command line arguments"""
     parser = argparse.ArgumentParser(
       description="Export Day One entries using a Jinja template",
-      usage="%(prog)s [--output FILE] [--timezone ZONE] [opts] journal",
+      usage="%(prog)s [--output FILE] [opts] journal",
       epilog="""If the Day One package has photos, you may need to copy
         the "photos" folder from the package into the same directory
         as the output file.""")
-    parser.add_argument('journal', help="path to Day One journal package",
-      nargs="?")
+    parser.add_argument('journal', help="path to Day One journal package")
     parser.add_argument('--output', metavar="FILE",
       help="file to write (default print to stdout)")
-    parser.add_argument('--timezone', metavar="ZONE",
-      help='time zone name. (--timezone "?" for more info)')
     parser.add_argument('--format', metavar="FMT",
       help="output format (default guess from output file extension)")
     parser.add_argument('--template', metavar="NAME",
@@ -42,40 +39,6 @@ def parse_args(args=None):
     return parser.parse_args(args)
 
 
-def timezone_help(s):
-    """Display help on time zone"""
-    if s == '?':
-        title, zones = "Common time zones:", times.pytz.common_timezones
-    elif s == "??":
-        title, zones = "All possible time zones:", times.pytz.all_timezones
-    elif len(s) == 3:
-        title = "Time zones for country: " + s[1:]
-        try:
-            zones = times.pytz.country_timezones(s[1:])
-        except KeyError:
-            return "Unrecognized country code: " + s[1:]
-    else:
-        return "Unrecognized option: --timezone " + s
-
-    print title
-    i = 0
-    for i, z in enumerate(zones):
-        if i % 2 or not sys.stdout.isatty():
-            print z
-        else:
-            print "{: <34}".format(z),
-    if not i % 2 and sys.stdout.isatty():
-        print
-
-    print """\
-For information about time zone choices use one of the following options:
-    --timezone "?"   print common time zones
-    --timezone "??"  print all time zones
-    --timezone "?XX" all time zones in country with two-letter code XX"""
-
-    return 0
-
-
 # command line interface
 def run(args=None):
     args = parse_args(args)
@@ -86,20 +49,6 @@ def run(args=None):
                       else 'html'
     if args.format.lower() in ['md', 'markdown', 'mdown', 'mkdn']:
         args.format = 'md'
-
-    if not args.timezone:
-        tz = 'utc'
-    elif args.timezone[0] == "?":
-        return timezone_help(args.timezone)
-    else:
-        try:
-            tz = times.pytz.timezone(args.timezone)
-        except times.pytz.UnknownTimeZoneError:
-            return "Unknown time zone: " + args.timezone
-
-    # Make sure there is a journal
-    if args.journal is None:
-        return "Error: too few arguments"
 
     # Check journal files exist
     args.journal = os.path.expanduser(args.journal)
@@ -115,7 +64,7 @@ def run(args=None):
 
     try:
         output = dayone_export(args.journal, template=args.template,
-          timezone=tz, reverse=args.reverse, tags=tags, after=args.after,
+          reverse=args.reverse, tags=tags, after=args.after,
           format=args.format, template_dir=args.template_dir)
     except jinja2.TemplateNotFound as err:
         return "Template not found: {0}".format(err)
