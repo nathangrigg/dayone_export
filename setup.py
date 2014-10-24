434f65890c9d94733ffaf6913828c00dcf09958e
Nathan Grigg <nathan@nathangrigg.net>
2014-10-24 08:20:41 -0700

    Set version to 0.8.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if sys.version_info < (2, 7) or (
 
 setup(
     name = "dayone_export",
-    version = '0.8.1',
+    version = '0.8.2',
     description = "Export Day One journal using Jinja2 templates",
     author = "Nathan Grigg",
     author_email = "nathan@nathangrigg.net",
