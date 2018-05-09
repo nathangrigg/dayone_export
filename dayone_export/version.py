548f75a95970f5df9315e3955ebd5e20d5ab2781
Nathan Grigg <nathan@nathangrigg.com>
2018-05-08 19:51:24 -0700

    Bump version to 1.0.0

--- dayone_export/version.py
+++ dayone_export/version.py
@@ -1,1 +1,1 @@
-VERSION = "0.8.3"
+VERSION = "1.0.0"
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if sys.version_info < (2, 7) or (
 
 setup(
     name = "dayone_export",
-    version = '0.8.3',
+    version = '1.0.0',
     description = "Export Day One journal using Jinja2 templates",
     author = "Nathan Grigg",
     author_email = "nathan@nathangrigg.net",
